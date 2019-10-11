369fe48091c9eb9d0ce6ef53ebc5915919bd8afe
komerela <kmwongera@icloud.com>
2019-10-11 05:25:50 +0000

    web stack debugging task, we are testing how well our web server setup featuring Nginx

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# puppet changes the ULIMIT for nginx
+exec { 'sed file':
+  command => "sed -i 's/15/4096/g' /etc/default/nginx; service nginx restart",
+  path    => ['/bin', '/usr/sbin', '/usr/bin'],
+}
